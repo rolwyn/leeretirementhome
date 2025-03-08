778d527407fddf6b166716858b6e168150de2037
rolwyn <rolwyn.quadras051@gmail.com>
2025-03-09 00:08:39 +0530

    initial work

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Home Page',
+}
+
+const HomePage = () => {
+  return <>Lee Retirement Home</>;
+}
+ 
+export default HomePage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Lee\'s Assisted Living';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Lee\'s Assisted Living For Seniors';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
